d54f7bc408ace627bc314a9fdec40c2f73d701cb
Matthew Reyes <mreyez@gmail.com>
2018-11-12 22:48:52 -0800

    Form validation messages
    Add Form validation messages inline

--- app/routes.py
+++ app/routes.py
@@ -23,5 +23,5 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index.html')
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
